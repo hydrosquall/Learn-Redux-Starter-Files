4324ca0db4c7e4e56f85e8ab059b13cf18a0bf0f
Cameron Yick <cameron.yick@yale.edu>
2016-08-19 21:46:36 -0400

    bound data to the view

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,12 +1,15 @@
 // The main application wrapper
 import React from 'react';
-import { Link } from 'react-router';
- 
+// import { Link } from 'react-router';
+import Photo from './Photo';
+
+// ... is ES6 spread... gives ALL the properties out. One could be more careful and selective about this actually.
+// Pass key and i since key is not usable in app (react consumes it)
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        I'm the photogrid
+        {this.props.posts.map( (post,i) => <Photo {... this.props} key={i} i={i} post={post} />)}
       </div>
       )
   }
